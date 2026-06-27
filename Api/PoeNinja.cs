03331e8e59f83cc98ce9d8d9bc3483d2639aa8b2
im_ny <tuyettran9066@gmail.com>
2026-06-27 16:26:15 +0700

    Update PoeNinja.cs

--- Api/PoeNinja.cs
+++ Api/PoeNinja.cs
@@ -8,7 +8,7 @@ namespace Beasts.Api;
 
 public static class PoeNinja
 {
-    private static readonly string PoeNinjaUrl = "https://poe.ninja/api/data/itemoverview?league=Mirage&type=Beast";
+    private static readonly string PoeNinjaUrl = "https://poe.ninja/api/data/itemoverview?league=Ancestors&type=Beast";
 
     private class PoeNinjaLine
     {
